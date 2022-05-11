939383c61c8ef0cc75ee8f71d946778dee8a6508
Nickolas Ribeiro de Mendonca <nickolasrm1@hotmail.com>
2022-05-11 15:13:05 -0300

    Maps implementados

---
+++ QuaseTresLanches2.0/src/academy/learnprogramming/Pedido.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming;
+
+
+public class Pedido {
+    private String pedidos;
+    private double valor;
+
+
+    public Pedido(String pedidos, double valor) {
+        this.pedidos = pedidos;
+        this.valor = valor;
+    }
+
+    public Pedido criarNovoPedido(String nome, double valor){
+        return new Pedido(nome,valor);
+    }
+
+    public String getPedidos() {
+        return pedidos;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+}
